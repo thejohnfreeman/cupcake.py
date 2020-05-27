3d4103395d574c29a983561f02af9143021ed89d
John Freeman <jfreeman08@gmail.com>
2020-05-27 11:52:01 -0500

    Log shell commands to stderr by default

--- cupcake/shell.py
+++ cupcake/shell.py
@@ -1,24 +1,27 @@
 """A pseudo-shell for nicer subprocesses.
 
 - Keep preferred defaults as state.
-- Echo the command to stdout.
+- Accept convertible-to-string arguments instead of just string-like.
+- Log the command to stderr by default.
 - TODO: Do not echo subprocess output unless there was an error.
 - Assert zero return code by default.
 """
 
 import shlex
 import subprocess
+import sys
 
 
 class Shell:
 
-    def __init__(self, **kwargs):
+    def __init__(self, *, log=sys.stderr, **kwargs):
         self.defaults = {'check': True, **kwargs}
+        self.log = log
 
     def run(self, command, *args, **kwargs):
         # Let callers pass `int`s. Just do the right thing.
         command = tuple(str(arg) for arg in command)
         kwargs = {**self.defaults, **kwargs}
         # TODO: Prefix with the CWD?
-        print('$ ' + ' '.join(shlex.quote(arg) for arg in command))
+        print('$ ' + ' '.join(shlex.quote(arg) for arg in command), file=self.log)
         return subprocess.run(command, *args, **kwargs)
