75cfe174bfc187d8a2ce8c07faa723a57737c3bc
John Freeman <jfreeman08@gmail.com>
2020-08-25 18:08:45 -0500

    Add tests: test and install project-template-cpp

--- tests/test_cupcake.py
+++ tests/test_cupcake.py
@@ -25,3 +25,41 @@ def test_build(project_template_cpp, tmp_path):
     assert subprocess.check_output(
         [tmp_path / 'debug' / 'src' / 'greet']
     ) == b'hello!\n'
+
+
+def test_test(project_template_cpp, tmp_path):
+    subprocess.check_call(
+        [
+            'cupcake',
+            'test',
+            '--source-directory',
+            project_template_cpp,
+            '--build-directory',
+            tmp_path,
+        ]
+    )
+
+
+def test_install(project_template_cpp, tmp_path):
+    prefix = tmp_path / 'install'
+    subprocess.check_call(
+        [
+            'cupcake',
+            'install',
+            '--source-directory',
+            project_template_cpp,
+            '--build-directory',
+            tmp_path / 'build',
+            '--prefix',
+            prefix,
+        ]
+    )
+    subprocess.check_call(
+        [
+            'diff',
+            project_template_cpp / 'include' / 'greetings' / 'greetings.hpp',
+            prefix / 'include' / 'greetings' / 'greetings.hpp',
+        ]
+    )
+    assert (prefix / 'lib' / 'libgreetings.a').is_file()
+    assert subprocess.check_output([prefix / 'bin' / 'greet']) == b'hello!\n'
