8470f32652d664ee571c6655d5d5067990a1e9e8
John Freeman <jfreeman08@gmail.com>
2023-04-03 15:15:51 -0500

    Add test for new project

---
+++ tests/test_new.py
@@ -0,0 +1,13 @@
+import subprocess
+import tempfile
+
+def test_new():
+    with tempfile.TemporaryDirectory() as work_dir:
+        def test(*args):
+            subprocess.check_call(['cupcake', *args], cwd=work_dir)
+        test('new', 'foo')
+        test('build', '-S', 'foo', '-B', '.build')
+        test('clean', '-B', '.build')
+        test('test', '-S', 'foo', '-B', '.build')
+        test('install', '-S', 'foo', '-B', '.build', '--prefix', '.install')
+        test('add', '-S', 'foo', 'boost')
